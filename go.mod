513f13052b57cd049583c1b2bfac5a48060e6cb0
Jacalz <jacalz@tutanota.com>
2025-10-29 17:43:08 +0100

    Initial tool version

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rymdport/gobinupd
 
 go 1.25.3
+
+require golang.org/x/sync v0.17.0
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"debug/buildinfo"
+	"io/fs"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("Failed to load user's home directory: %v", err)
+	}
+
+	gobin := filepath.Join(home, "go", "bin")
+	err = os.Chdir(gobin)
+	if err != nil {
+		log.Fatalf("Failed to change directory to %s: %v", gobin, err)
+	}
+
+	binaries, err := os.ReadDir(gobin)
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+
+	wg := errgroup.Group{}
+	for _, binary := range binaries {
+		wg.Go(func() error {
+			if binary.IsDir() {
+				return nil
+			}
+
+			return installLatestVersionOf(binary)
+		})
+	}
+
+	if err := wg.Wait(); err != nil {
+		log.Fatalf("Failed to install latest version: %v", err)
+	}
+}
+
+func installLatestVersionOf(binary fs.DirEntry) error {
+	name := binary.Name()
+	info, err := buildinfo.ReadFile(name)
+	if err != nil {
+		log.Printf("Failed to read build info for %s: %v", name, err)
+		return err
+	}
+
+	cmd := exec.Command("go", "install", info.Path+"@latest")
+	return cmd.Run()
+}
